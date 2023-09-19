96aea381b1dc8b285ce7eccad29c5abf09b58f2b
Arvi-H <arvihaxhillari1@gmail.com>
2023-09-19 10:22:53 -0600

    testing git connection

--- fermat.py
+++ fermat.py
@@ -4,7 +4,7 @@ import random
 def prime_test(N, k):
     # This is the main function connected to the Test button. You don't need to touch it.
     return run_fermat(N,k), run_miller_rabin(N,k)
-
+ 
 
 def mod_exp(x, y, N):
     # You will need to implement this function and change the return value.   
